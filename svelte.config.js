41643128ba96854078222b01e10d1110f82b77a0
Harrison Doran <harrydoran@pm.me>
2024-10-26 16:30:16 +1300

    Change build output directory

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,13 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'docs',
+			assets: 'docs',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
 	},
 	base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 };
